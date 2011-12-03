c639f29702db8ccae0a61f48c192614743868ab0
Douglas Alves <dnalves3@gmail.com>
2011-12-03 12:51:12 -0200

    Fetch feito. Fiz um Mux2 tambem.

--- mips.vhd
+++ mips.vhd
@@ -9,16 +9,19 @@ entity mips is
 	);
 	port(
 		Instruction : in std_logic_vector(nbits -1 downto 0);
-		Data				: in std_logic_vector(nbits -1 downto 0);
-		clk 				: in std_logic;
-		reset				: in std_logic;
-		PCF					: out std_logic_vector(nbits -1 downto 0);
-		ALUOutM			: out std_logic_vector(nbits -1 downto 0);
+		Data		: in std_logic_vector(nbits -1 downto 0);
+		clk 		: in std_logic;
+		reset		: in std_logic;
+		PCF			: out std_logic_vector(nbits -1 downto 0);
+		ALUOutM		: out std_logic_vector(nbits -1 downto 0);
 		WriteDataM	: out std_logic_vector(nbits -1 downto 0);
-		MemWriteM		: out std_logic
+		MemWriteM	: out std_logic
 	);
 end mips;
   
-architecture arc_mips of mips is begin
+architecture arc_mips of mips is
+
+	
+begin
 
 end;
