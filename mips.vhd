f96f6f5ee40f81bc0c44166784978eb978cd20de
Bruno Buccolo <bruno.buccolo@gmail.com>
2011-12-04 02:59:09 -0200

    Anotando o funcionamento da simulacao, conforme conversei com o Tasso.

--- mips.vhd
+++ mips.vhd
@@ -8,12 +8,27 @@ entity mips is
 		nbits : positive := 32
 	);
 	port(
+		-- A simulacao ira funcionar da seguinte maneira
+		-- 1. O sinal reset serah ligado, para colocar o datapath no estado
+		--    inicial. Isso inclui resetar o RegFile e colocar o PC = 0.
+		-- 2. Agora podemos executar normalmente.
+
+		-- Instruction Memory: Leitura
 		Instruction : in std_logic_vector(nbits -1 downto 0);
+
+		-- Data Memory: Leitura	
 		Data		: in std_logic_vector(nbits -1 downto 0);
+
 		clk 		: in std_logic;
 		reset		: in std_logic;
+
+		-- PCF: Fizemos os jumps / branches corretamente?
 		PCF			: out std_logic_vector(nbits -1 downto 0);
+		
+		-- ALU: A ALU esta funcionando? 
 		ALUOutM		: out std_logic_vector(nbits -1 downto 0);
+
+		--  Data Memory: Escrita
 		WriteDataM	: out std_logic_vector(nbits -1 downto 0);
 		MemWriteM	: out std_logic
 	);
