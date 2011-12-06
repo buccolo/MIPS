ddd18056e5d033a92725bdd0fa557f08c702f919
Bruno Buccolo <bruno.buccolo@gmail.com>
2011-12-06 16:59:04 -0200

    Teste: 3xLW, ADD e AND por enquanto.

--- tb_mips.vhd
+++ tb_mips.vhd
@@ -13,13 +13,13 @@ component mips is
 	);
 	port(
 		Instruction : in std_logic_vector(nbits -1 downto 0);
-		Data				: in std_logic_vector(nbits -1 downto 0);
-		clk 				: in std_logic;
-		reset				: in std_logic;
-		PCF					: out std_logic_vector(nbits -1 downto 0);
-		ALUOutM			: out std_logic_vector(nbits -1 downto 0);
+		Data		: in std_logic_vector(nbits -1 downto 0);
+		clk 		: in std_logic;
+		reset		: in std_logic;
+		PCF			: out std_logic_vector(nbits -1 downto 0);
+		ALUOutM		: out std_logic_vector(nbits -1 downto 0);
 		WriteDataM	: out std_logic_vector(nbits -1 downto 0);
-		MemWriteM		: out std_logic
+		MemWriteM	: out std_logic
 	);
 end component;
 
@@ -38,50 +38,78 @@ signal ALUOutM		: std_logic_vector(31 downto 0);
 signal WriteDataM	: std_logic_vector(31 downto 0);
 signal MemWriteM	: std_logic;
 
+-- RTYPE  RD, RS, RT --------------------+
+-- --------------------------------------|
+-- OP     RS    RT    RD    SHIFT FUNCT  |
+-- 000000 SSSSS TTTTT DDDDD 00000 000000 |
+-----------------------------------------+
+
+-- LW RT, IMEDIATE(RS) -------------------+
+-- ---------------------------------------|
+-- OP     RS    RT    IMEDIATE            |
+-- 100011 00000 01010 0000 0000 0010 0000 |
+------------------------------------------+
+
+-- SW RT, IMEDIATE(RS) -------------------+
+-- ---------------------------------------|
+-- OP     RS    RT    IMEDIATE            |
+-- 101011 00000 01010 0000 0000 0010 0000 |
+------------------------------------------+
+
+-- BEQ RS, RT, IMEDIATE ------------------+
+-- ---------------------------------------|
+-- OP     RS    RT    IMEDIATE            |
+-- 000100 00000 01010 0000 0000 0010 0000 |
+------------------------------------------+
+
+-- J 26-BIT-IMEDIATE ----------------+
+-- ----------------------------------|
+-- OP     26 BIT IMEDIATE            |
+-- 000010 00000000000000000000000000 |
+-------------------------------------+
+
+-- JAL 26-BIT-IMEDIATE --------------+
+-- ----------------------------------|
+-- OP     26 BIT IMEDIATE            |
+-- 000011 00000000000000000000000000 |
+-------------------------------------+
+
 begin
 	mips_0: mips port map (Instruction, Data, clk, reset, PCF, AluOutM, WriteDataM, MemWriteM);
 
 	clk <= not clk after 50 ns;
 
-	process
+	process begin
 		reset <= '1';
 		wait for 50 ns;
 
-		-- RTYPE  RD, RS, RT --------------------+
-		-- --------------------------------------|
-		-- OP     RS    RT    RD    SHIFT FUNCT  |
-		-- 000000 00000 00000 00000 00000 000000 |
-        -----------------------------------------+
+		-- lw $1, 1($0) >>> $0, $1, imediate
+		Instruction <= "100011"&"00000"&"00001"&"0000000000000001";
+		Data <= "10000000000000000000000000000000";
+		assert PCF = "00000000000000000000000000000100" report "PC NAO FUNCIONA";
+		wait for 50 ns;
 
-		-- LW RT, IMEDIATE(RS) -------------------+
-		-- ---------------------------------------|
-		-- OP     RS    RT    IMEDIATE            |
-		-- 100011 00000 01010 0000 0000 0010 0000 |
-        ------------------------------------------+
+		-- lw $2, 2($0) 
+		Instruction <= "100011"&"00000"&"00010"&"0000000000000010";
+		Data <= "01111111111111111111111111111111";
+		assert PCF = "00000000000000000000000000001000" report "PC NAO FUNCIONA";
+		wait for 50 ns;
 
-		-- SW RT, IMEDIATE(RS) -------------------+
-		-- ---------------------------------------|
-		-- OP     RS    RT    IMEDIATE            |
-		-- 101011 00000 01010 0000 0000 0010 0000 |
-        ------------------------------------------+
+		-- lw $3, 3($0) 
+		Instruction <= "100011"&"00000"&"00011"&"0000000000000011";
+		Data <= "00000000000000001111111111111111";
+		assert PCF = "00000000000000000000000000001100" report "PC NAO FUNCIONA";
+		wait for 50 ns;
 
-		-- BEQ RS, RT, IMEDIATE ------------------+
-		-- ---------------------------------------|
-		-- OP     RS    RT    IMEDIATE            |
-		-- 000100 00000 01010 0000 0000 0010 0000 |
-        ------------------------------------------+
+		-- add $4, $1, $2
+		Instruction <= "000000"&"00001"&"00010"&"00100"&"00000"&"100000";
+		assert PCF = "00000000000000000000000000010000" report "PC NAO FUNCIONA";
+		wait for 50 ns;
 
-		-- J 26-BIT-IMEDIATE ----------------+
-		-- ----------------------------------|
-		-- OP     26 BIT IMEDIATE            |
-		-- 000010 00000000000000000000000000 |
-        -------------------------------------+
-
-		-- JAL 26-BIT-IMEDIATE --------------+
-		-- ----------------------------------|
-		-- OP     26 BIT IMEDIATE            |
-		-- 000011 00000000000000000000000000 |
-        -------------------------------------+
+		-- and $5, $3, $2 >>> $3, $2, $5
+		Instruction <= "000000"&"00011"&"00010"&"00110"&"00000"&"100100";
+		assert PCF = "00000000000000000000000000010100" report "PC NAO FUNCIONA";
+		wait for 50 ns;
 
 	end process;
 	
